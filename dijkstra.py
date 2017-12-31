7f16fbc65c30314a41c5d5b502e271c1c592637f
tati-z <tatianazihindula@gmail.com>
2017-12-31 21:26:07 +0000

    added the parent table

--- dijkstra.py
+++ dijkstra.py
@@ -1,7 +1,7 @@
 # initialise the graph
 graph = dict() # the whole graph is a dictionary
 
-# the graph contain the node start
+# the graph contain the node 'start'
 
 graph['start'] = {} # the start has two children nodes 'a' and 'b'
 graph['start']['a'] = 6 # it takes 6 units to go from start to a
@@ -14,21 +14,22 @@ graph['start']['b'] = 2 # it takes 2 units to go from start to b
 
 '''
 
-# the graph also contain the node start
+# the graph also contain the node 'a'
 graph['a'] = {}
 graph['a']['end'] = 1 # it takes one unit to go from node 'a' to the end
 
-# this graph also contains the node b
+# this graph also contains the node 'b'
 graph['b'] = {}
 graph['b']['a'] = 3 # it takes 3 units to go from 'b' to 'a'
 graph['b']['end'] = 5 # it takes 5 units to go to the end from when at b
 
-# the last node contained in the graph is the end
+# the last node contained in the graph is the 'end'
 graph['end'] = {} # the end node points to nothing. i.e it doesn't have a neighbours.
 
 
 # next create a hastable to store the cost of each node
-# this is the how many units it takes to come to this node counted from graph['start']
+# this is the how many units it takes to come to this node counted from first node used
+
 # e.g from start to 'a' it takes 6 units, and from start to 'b' it takes 2 units.
 # but as the transversal continues this will change.
 # e.g from start then b then 'a' the cost to get to 'a' will be 5, this is the '5' it
@@ -41,3 +42,11 @@ infinity = float('inf')
 cost['a'] = 6
 cost['b'] = 2
 cost['end'] = infinity
+
+# add the parents. This will add the parent of each node. usefull for bacjtracking
+
+parent = {}
+parent['a'] = 'start'
+parent['b'] = 'start'
+parent['end'] = Node
+
