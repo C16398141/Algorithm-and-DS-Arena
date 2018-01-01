dee85e7bc9ee96b4a87d5bcd96eb50273c834842
tati-z <tatianazihindula@gmail.com>
2018-01-01 13:35:32 +0000

    added heap method prototypes

--- heap.py
+++ heap.py
@@ -1,7 +1,7 @@
-
+# O log(n)
 def is_heap(array):
+	'''return true if the array given has heap properties'''
 	
-	'''return true if hip_array has heap properties'''
 	for i in range(1,(len(array)//2)+1):
 		if (2*i)+1 < len(array):
 			if array[i] < array[2*i] or array[i] < array[(2*i) +1]:
@@ -11,3 +11,12 @@ def is_heap(array):
 		print('i',i,'i*2',2*i,'i*2+1')
 		
 	return True
+
+def build_head(array, MIN=True):
+	pass
+def heapify(array, MIN=True):
+	pass
+def delete_top(array):
+	pass
+def insert(array):
+	pass
